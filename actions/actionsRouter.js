cc821fe27359e5152e4b3b0f1495f361bd8f14b5
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-01 09:50:23 -0700

    added module.export to router files

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -11,13 +11,15 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/:id", (req, res) => {
-    
+
 });
 
 router.put("/:id", (req, res) => {
-    
+
 });
 
 router.delete("/:id", (req, res) => {
-    
+
 });
+
+module.exports = router;
